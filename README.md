77710c2c7a59252deb10054cb5443baeb8488673
JJ <jjjay@me.com>
2015-10-22 14:45:14 -0500

    now runs from a cron job and emails are html
    This version is same as one running in production.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a simple plugin which replaces bbp_notify_subscribers. It adds in a one click unsubscribe link as well as a link to the user's subscription page. To avoid confusion, the unsubscribe link won't work if a user is logged in with a different account. However, it will work if a user is not logged on and, as you might expect, if they are logged on with account to whom the email was sent. It also adds a clear reminder that it's not possible to post by email.
 
-The email sends to one subscriber at a time, the same way as bbPress <=2.5.3.
+The email sends to one subscriber at a time but queued in a cron job. The emails are now html.
--- mjj-bbp-subscription.php
+++ mjj-bbp-subscription.php
@@ -1,10 +1,10 @@
 <?php
 /*
-Plugin Name: MJJ bbP Subscription
-Description: Custom subscribe to topic email with unsubscribe link 
-Author: Mary (JJ) Jay
-Version: 1.0
-License: GPLv2 or later: http://www.gnu.org/licenses/licenses.html
+* Plugin Name: MJJ BBP Subscription
+* Description: Custom subscribe to topic email with unsubscribe link. Cron job added using https://bbpress.org/forums/topic/hook-to-bbpress-notifications/#post-156426 by @korobochkin
+* Author: Mary (JJ) Jay
+* Version: 1
+* License: GPLv2 or later: http://www.gnu.org/licenses/licenses.html
 */
 
 //this makes the salt for the check if needed. If there is one, it will not make one.
@@ -12,10 +12,33 @@ register_activation_hook( __FILE__, 'mjj_salt_activate' );
 
 //custom topic subscription emails
 remove_action( 'bbp_new_reply',    'bbp_notify_subscribers', 11, 5 );
-add_action( 'bbp_new_reply',    'mjj_bbp_notify_subscribers', 11, 5 );
+remove_action( 'bbp_new_reply',    'bbp_notify_topic_subscribers', 11, 5 );
+remove_action( 'bbp_new_topic',    'bbp_notify_forum_subscribers', 11, 4 );
+
+// when a new reply is created, create the cron job
+add_action( 'bbp_new_reply',    'mjj_bbp_notify_subscribers_cron', 11, 5 );
+// the hook in the cron is the notify subscription function itself
+add_action( 'mjj_bbp_notify_subscribers_callback',    'mjj_bbp_notify_subscribers', 11, 5 );
 
 //add the one click unsubscribe on the topic itself
 add_action( 'bbp_template_before_single_topic', 'mjj_bbp_unsubscribe_notice' );
+
+// the cron job setup
+function mjj_bbp_notify_subscribers_cron( $reply_id = 0, $topic_id = 0, $forum_id = 0, $anonymous_data = false, $reply_author = 0 ){
+	wp_schedule_single_event (
+		time (),
+		'mjj_bbp_notify_subscribers_callback',
+		array (
+			$reply_id,
+			$topic_id,
+			$forum_id,
+			$anonymous_data,
+			$reply_author
+		)
+	);
+
+	return true;
+}
 
 //I don't think activation hooks are allowed return values?
 function mjj_salt_activate(){
@@ -25,51 +48,52 @@ function mjj_salt_activate(){
 
 //let's make a random value and add it to the database so it doesn't change and the links will always work
 function mjj_get_salt() {
-	
+
 	$salt = get_option( 'mjj_bbp_unsub_salt' );
 	if( !$salt ){
 		$salt = base64_encode( mcrypt_create_iv(12, MCRYPT_DEV_URANDOM) );
 		//add the salt to the options table, it should not change
-		add_option( 'mjj_bbp_unsub_salt', $salt ); 
+		add_option( 'mjj_bbp_unsub_salt', $salt );
 	}
 	return $salt;
-	
+
 }
 
 
-function mjj_not_a_nonce( $uid, $tid ){
-//this is used for a quick check that the url hasn't been tampered with. It's not dependent on time or whether or not a user is logged in.	
-	
+function not_a_nonce( $uid, $tid ){
+//this is used for a quick check that the url hasn't been tampered with. It's not dependent on time or whether or not a user is logged in.
 	$salt = mjj_get_salt();
 	//because why not add a little string to it
 	$string = $uid . $tid . "user and topic ids";
 	$encrypted = crypt( $string, $salt );
-	return $encrypted;	
+	return $encrypted;
 
 }
 
-/** 
+/**
 * from bbpress / includes / common / functions.php
 * this is taken from 2.5.3, except for the unsubsribe stuff
 **/
 function mjj_bbp_notify_subscribers( $reply_id = 0, $topic_id = 0, $forum_id = 0, $anonymous_data = false, $reply_author = 0 ) {
 
 	// Bail if subscriptions are turned off
-	if ( !bbp_is_subscriptions_active() )
+	if ( !bbp_is_subscriptions_active() ){
 		return false;
+	}
 
 	/** Validation ************************************************************/
 
 	$reply_id = bbp_get_reply_id( $reply_id );
 	$topic_id = bbp_get_topic_id( $topic_id );
 	$forum_id = bbp_get_forum_id( $forum_id );
-	
+
 
 	/** Reply *****************************************************************/
 
 	// Bail if reply is not published
-	if ( !bbp_is_reply_published( $reply_id ) )
+	if ( !bbp_is_reply_published( $reply_id ) ){
 		return false;
+	}
 
 	/** Topic *****************************************************************/
 
@@ -77,15 +101,16 @@ function mjj_bbp_notify_subscribers( $reply_id = 0, $topic_id = 0, $forum_id = 0
 	if ( !bbp_is_topic_published( $topic_id ) ){
 		return false;
 	}
-	
+
 	$topic_url = bbp_get_topic_permalink( $topic_id );
-	
+
 	/** User ******************************************************************/
 
 	// Get topic subscribers and bail if empty
 	$user_ids = bbp_get_topic_subscribers( $topic_id, true );
-	if ( empty( $user_ids ) )
+	if ( empty( $user_ids ) ){
 		return false;
+	}
 
 	// Poster name
 	$reply_author_name = bbp_get_reply_author_display_name( $reply_id );
@@ -108,10 +133,13 @@ function mjj_bbp_notify_subscribers( $reply_id = 0, $topic_id = 0, $forum_id = 0
 
 	// Loop through users
 	foreach ( (array) $user_ids as $user_id ) {
-	
-		$nn_link 	   = mjj_not_a_nonce( $user_id, $topic_id );
+
+		$recipient     = get_userdata( $user_id );
+		$recipient_name= $recipient->user_login;
+
+		$nn_link 	   = not_a_nonce( $user_id, $topic_id );
 		$unsubscribe_link = $topic_url . '?uid=' . $user_id . '&tid=' . $topic_id . '&nn=' . $nn_link;
-		
+
 		$user_sub_link = bbp_get_user_profile_url( $user_id ) . 'subscriptions/';
 
 		// Don't send notifications to the person who made the post
@@ -119,11 +147,11 @@ function mjj_bbp_notify_subscribers( $reply_id = 0, $topic_id = 0, $forum_id = 0
 			continue;
 
 		// For plugins to filter messages per reply/topic/user
-		$message = sprintf( __( 'Hi,
+		$message = sprintf( __( 'Hi %8$s,
 
-You are receiving this email because you have subscribed to the topic "%6$s." Links to unsubscribe are below.
+You are receiving this email because you have subscribed to the topic <a href="%3$s">%6$s</a>. Links to unsubscribe are below.
 
-You can\'t reply to the topics via email, so you\'ll need to go to the topic on the site here: %3$s
+You can\'t reply to the topics via email, so you\'ll need to go to the topic on the site here: <a href="%3$s">%3$s</a>
 ======
 
 %1$s wrote on %6$s:
@@ -132,9 +160,9 @@ You can\'t reply to the topics via email, so you\'ll need to go to the topic on 
 
 ======
 
-Unsubscribe in one click here: %4$s
-Manage all your subscriptions: %5$s
-If you have any issues, please email %7$s  
+Unsubscribe in one click here: <a href="%4$s">%4$s</a>
+Manage all your subscriptions: <a href="%5$s">%5$s</a>
+If you have any issues, please email <a href="mailto:%7$s">%7$s</a>
 ', 'bbpress' ),
 
 			$reply_author_name,
@@ -143,7 +171,8 @@ If you have any issues, please email %7$s
 			$unsubscribe_link,
 			$user_sub_link,
 			$topic_title,
-			$admin_email
+			$admin_email,
+			$recipient_name
 		);
 
 		$message = apply_filters( 'bbp_subscription_mail_message', $message, $reply_id, $topic_id, $user_id );
@@ -172,14 +201,14 @@ If you have any issues, please email %7$s
 
 //this function does the work of the unsubscribing and also gives a little template notice. It's on the topic in question - I thought about putting it on the user profile page but don't want to give out that info if the url is shared
 function mjj_bbp_unsubscribe_notice(){
-	
+
 	global $post;
 
 	$topic_id = ( isset( $_GET['tid'] ) && !empty($_GET['tid'] ) ? $_GET['tid'] : '' );
 	$user_id = ( isset( $_GET['uid'] ) && !empty($_GET['uid'] ) ? $_GET['uid'] : '' );
 	$to_check = ( isset( $_GET['nn'] ) && !empty($_GET['nn'] ) ? $_GET['nn'] : '' );
-	$check = mjj_not_a_nonce( $user_id, $topic_id );
-	
+	$check = not_a_nonce( $user_id, $topic_id );
+
 	$current_user = is_user_logged_in() ? wp_get_current_user() : false;
 
 	//are there any of the query vars? If not, return
@@ -188,27 +217,27 @@ function mjj_bbp_unsubscribe_notice(){
 	}
 	//if you fail the check, it  doesn't work. Also it has to be on the right topic.
 	elseif( $to_check != $check || $post->ID != $topic_id  ){
-		$message = 'Something has gone wrong. You can manage your subscriptions from your profile.'; 
+		$message = 'Something has gone wrong. You can manage your subscriptions from your profile.';
 	}
 	//if you are logged in and using someone else's link, it won't work for you (or unsubscribe them) so you get a link to your subscriptions page
 	elseif( is_user_logged_in() && $current_user-> ID != $user_id ){
-		
+
 		$subscriptions_link = bbp_get_user_profile_url( $current_user->ID ) . 'subscriptions/';
 		$message = 'Unsubscribe links are unique to each user of the forum.<br />This link can&rsquo;t be used for you to unsubscribe, but you can manage all your subscriptions <a href="' . $subscriptions_link . '">here</a>';
-		
+
 	}
 	//what else haven't I thought about? add anything I've forgotten here. If you are not logged in, you can still unsubscribe. That's ok. Better accidental unsubscriptions than people not being able to unsubscribe.
 	else{
-	
+
 		bbp_remove_user_subscription( $user_id, $topic_id );
 		$message = 'You have been unsubscribed from this topic.';
-	
+
 	}
-	
+
 	echo '<div class="bbp-template-notice">';
 	echo $message;
-	echo '</div>';	
-	
+	echo '</div>';
+
 	return;
 
 }
